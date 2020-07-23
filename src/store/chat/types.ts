13fc17694be78a5984dd41d78ed570a91925c717
jonathas fonseca <jonathas09@gmail.com>
2020-07-23 19:53:09 -0300

    actions chat criadas

--- src/store/chat/types.ts
+++ src/store/chat/types.ts
@@ -7,3 +7,20 @@ export interface Message {
 export interface ChatState {
   messages: Message[]
 }
+
+export const SEND_MESSAGE = 'SEND_MESSAGE'
+export const DELETE_MESSAGE = 'DELETE_MESSAGE'
+
+interface SendMessageAction {
+  type: typeof SEND_MESSAGE
+  payload: Message
+}
+
+interface DeleteMessageAction {
+  type: typeof DELETE_MESSAGE
+  meta: {
+    timestamp: number
+  }
+}
+
+export type ChatActionTypes = SendMessageAction | DeleteMessageAction
