bb416b2f88fba76fd5e5d4f798b918e2544bc9ba
jonathas fonseca <jonathas09@gmail.com>
2020-07-24 00:20:27 -0300

    final da configuraçao inicial do redux, adicionando useSelector no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {useSelector} from "react-redux";
+import { SystemState } from './store/system/types'
 function App() {
+  const user = useSelector<SystemState, SystemState['userName']>((state => state.userName))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{user}</h1>
     </div>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { systemReducer } from "./system/reducers"
 import { chatReducer } from "./chat/reducers"
 import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
   system: systemReducer,
   chat: chatReducer
 })
